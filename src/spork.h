1847a8ae07c354009978d27cb1e507685e04d552
sandakersmann <mkjaerstad@yahoo.no>
2014-12-17 02:47:57 +0100

    Added "Core" to copyright headers
    Github-Pull: #5494
    Rebased-From: 15de949bb9277e442302bdd8dee299a8d6deee60
    (cherry picked from commit f914f1a746d7f91951c1da262a4a749dd3ebfa71)

--- src/spork.h
+++ src/spork.h
@@ -1,5 +1,5 @@
 
-// Copyright (c) 2009-2012 The Crown developers
+// Copyright (c) 2009-2012 Crown Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef SPORK_H
