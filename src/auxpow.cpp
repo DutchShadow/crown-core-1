4fa0836e61c01e8577be1c59d67a132c5752b8cc
Wladimir J. van der Laan <laanwj@gmail.com>
2014-12-19 19:23:20 +0100

    Merge pull request #5470
    78253fc Remove references to X11 licence (Michael Ford)
    
    (cherry picked from commit 111a7d45f17fccdbff05a6afec222fe61684b860)

--- src/auxpow.cpp
+++ src/auxpow.cpp
@@ -2,7 +2,7 @@
 // Copyright (c) 2011 Vince Durham
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014 Daniel Kraft
-// Distributed under the MIT/X11 software license, see the accompanying
+// Distributed under the MIT software license, see the accompanying
 // file license.txt or http://www.opensource.org/licenses/mit-license.php.
 
 #include "auxpow.h"
